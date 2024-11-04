c8dfb83018e898cf626672a0442c5613eb6cb55d
Ansalps <ansalps10@gmail.com>
2024-11-04 16:59:57 +0530

    removed unique constraint for email

--- internal/models/userModel.go
+++ internal/models/userModel.go
@@ -9,7 +9,7 @@ type User struct {
 	//ID        uint   `gorm:"primary key" json:"id"`
 	FirstName string `json:"first_name" validate:"required"`
 	LastName  string `json:"last_name" validate:"required"`
-	Email     string `json:"email" gorm:"unique" validate:"required"`
+	Email     string `json:"email"  validate:"required"`
 	Password  string `json:"password" validate:"required"`
 	Phone     string `json:"phone" validate:"required,numeric,len=10"`
 	Status    string `gorm:"type:varchar(10); check(status IN ('Active', 'Blocked', 'Deleted')) ;default:'Active'" json:"status"`
